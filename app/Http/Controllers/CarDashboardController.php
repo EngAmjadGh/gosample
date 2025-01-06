ee612d77a5380026846c0662a7eef323618460a9
Amjad Ghneim <eng.amjadgh@gmail.com>
2025-01-06 17:28:11 +0300

    Update CarDashboardController.php

--- app/Http/Controllers/CarDashboardController.php
+++ app/Http/Controllers/CarDashboardController.php
@@ -31,59 +31,90 @@ class CarDashboardController extends Controller
     public function index()
     {
         abort_if(Gate::denies('car-dashboard'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        $imeis = Car::where('afaqi', true)->limit(3)->pluck('imei')->toArray();
+        $imeis = Car::where('afaqi', true)->pluck('imei')->toArray();
 
         $cars = [];
         $token = $this->generateAndSaveToken();//'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2FwaS5hZmFxeS5zYS9hdXRoL2xvZ2luIiwiaWF0IjoxNjkxOTIyMzU3LCJleHAiOjE2OTQ1MTQzNTcsIm5iZiI6MTY5MTkyMjM1NywianRpIjoiSjVMQUpPMk50MUtQZW1XMCIsInN1YiI6IjYyNDFhNTAyYWE1MGM4NjY1NTdkZjA1YiIsInBydiI6IjI3MGVjZmM3ZWEzZWQ5MzdlYTg0OTM2MmEzYTUwOTEwYzZkOGNlNGYifQ.PnxbuNX6TOVhSUzmQRiVKVEYhsatZ8fxg_6koYBni5A';
         // $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vYXBpLmFmYXF5LnNhL2F1dGgvbG9naW4iLCJpYXQiOjE3MzYwMTI2OTQsImV4cCI6MTczODYwNDY5NCwibmJmIjoxNzM2MDEyNjk0LCJqdGkiOiIydURlMXF4aEhGcVNvOHNpIiwic3ViIjoiNjVmOTRiM2QxNTRmNTExYTZkMDAyZjIyIiwicHJ2IjoiMjcwZWNmYzdlYTNlZDkzN2VhODQ5MzYyYTNhNTA5MTBjNmQ4Y2U0ZiJ9.CTehf02lax9werqBjr2m4biWpQaJFXRdls_F6di7jCs";
-        foreach ($imeis as $imei) {
-            // $afaqiVehicleId = '357073294755919';
-            $afaqiVehicleId = $imei;
-            $response = $this->getVehicleData($token, $afaqiVehicleId);
-            // \Log::info( $response);
-            if ($response) {
-                // decode the JSON response
-                $data = $response;
-                // $data = json_decode($response, true);
+        // dd($imeis);
+        $data = $this->getVehicleDataCustom($token, $imeis);
+        if($data) {
+            foreach ($data['data'] as $vehicle)
+            {
+                // create an array for the car
+                $car = [
+                    'id' => $vehicle['id'],
+                    'name' => $vehicle['n'],
+                    'i' => $vehicle['i'],
+                    'profile' => $vehicle['profile'],
+                    'sensors' => []
+                ];
+                $tempSensors = array_filter($vehicle['sensors'], function($sensor) {
+                    return $sensor['t'] == 'temperature';
+                    // return $sensor['n'] == 'TEMP1' || $sensor['n'] == 'TEMP2' || $sensor['n'] == 'TEMP3';
+                });
+                // foreach ($tempSensors as $sensor) {
+                //     // add the sensor to the car's sensors array
+                //     $car['sensors'][] = $sensor;
+                // }
+                // iterate through each sensor in the vehicle
+                $car['sensors'] = $tempSensors;
+                $cars[] = $car;
 
-                if ($data['status_code'] == 200) {
-                    // create an empty array to store grouped sensors
+                // \Log::alert($cars);
 
-                    // iterate through each vehicle in the response
-                    foreach ($data['data'] as $vehicle)
-                    {
-                        // create an array for the car
-                        $car = [
-                            'id' => $vehicle['id'],
-                            'name' => $vehicle['n'],
-                            'i' => $vehicle['i'],
-                            'profile' => $vehicle['profile'],
-                            'sensors' => []
-                        ];
-                        $tempSensors = array_filter($vehicle['sensors'], function($sensor) {
-                            return $sensor['t'] == 'temperature';
-                            // return $sensor['n'] == 'TEMP1' || $sensor['n'] == 'TEMP2' || $sensor['n'] == 'TEMP3';
-                        });
-                        // foreach ($tempSensors as $sensor) {
-                        //     // add the sensor to the car's sensors array
-                        //     $car['sensors'][] = $sensor;
-                        // }
-                        // iterate through each sensor in the vehicle
-                        $car['sensors'] = $tempSensors;
-                        $cars[] = $car;
-
-                        // \Log::alert($cars);
-
-                    }
-                } else {
-                    // handle error here
-                    \Log::info("stauts code <> 200");
-                }
-            } else {
-                // handle error here
-                \Log::info("failed response");
             }
+        } else {
+            \Log::info("failed response");
         }
+        // foreach ($imeis as $imei) {
+        //     // $afaqiVehicleId = '357073294755919';
+        //     $afaqiVehicleId = $imei;
+        //     $response = $this->getVehicleData($token, $afaqiVehicleId);
+        //     // \Log::info( $response);
+        //     if ($response) {
+        //         // decode the JSON response
+        //         $data = $response;
+        //         // $data = json_decode($response, true);
+
+        //         if ($data['status_code'] == 200) {
+        //             // create an empty array to store grouped sensors
+
+        //             // iterate through each vehicle in the response
+        //             foreach ($data['data'] as $vehicle)
+        //             {
+        //                 // create an array for the car
+        //                 $car = [
+        //                     'id' => $vehicle['id'],
+        //                     'name' => $vehicle['n'],
+        //                     'i' => $vehicle['i'],
+        //                     'profile' => $vehicle['profile'],
+        //                     'sensors' => []
+        //                 ];
+        //                 $tempSensors = array_filter($vehicle['sensors'], function($sensor) {
+        //                     return $sensor['t'] == 'temperature';
+        //                     // return $sensor['n'] == 'TEMP1' || $sensor['n'] == 'TEMP2' || $sensor['n'] == 'TEMP3';
+        //                 });
+        //                 // foreach ($tempSensors as $sensor) {
+        //                 //     // add the sensor to the car's sensors array
+        //                 //     $car['sensors'][] = $sensor;
+        //                 // }
+        //                 // iterate through each sensor in the vehicle
+        //                 $car['sensors'] = $tempSensors;
+        //                 $cars[] = $car;
+
+        //                 // \Log::alert($cars);
+
+        //             }
+        //         } else {
+        //             // handle error here
+        //             \Log::info("stauts code <> 200");
+        //         }
+        //     } else {
+        //         // handle error here
+        //         \Log::info("failed response");
+        //     }
+        // }
 
 
         return view('car-dashboard',[
@@ -120,6 +151,39 @@ public function getVehicleData($token, $afaqiVehicleId) {
     if ($response->ok()) {
         return $response->json();
     } else {
+        return null; // handle error here
+    }
+}
+public function getVehicleDataCustom($token, $afaqiVehicles) {
+    $url = 'https://api.afaqy.sa/units/lists?token=' . $token;
+    $response = Http::withoutVerifying()
+    // ->retry(3, 1000)
+    // ->timeout(30)
+    ->post($url, [
+        'data' => [
+            'simplify' => 1,
+            'filters' => [
+                'imei' => [
+                    'value' => $afaqiVehicles,
+                    'op' => 'in'
+                ],
+            ],
+            'projection' => [
+                'basic',
+                'last_update',
+                'sensors_last_val',
+                'counters',
+                'sensors'
+            ]
+        ]
+    ]);
+    if ($response->status() == 200) {
+        return $response->json();
+    } else {
+        // dd([
+        //     'status' => $response->status(),
+        //     'error' => $response->body(),
+        // ]);
         return null; // handle error here
     }
 }
