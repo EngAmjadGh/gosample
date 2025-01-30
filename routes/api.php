a09a7ac11b20bbcf0b82ee96cabf0dfabf4bc2d6
Amjad Ghneim <elit.it.app@gmail.com>
2025-01-30 13:45:16 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -106,4 +106,4 @@ Route::post('drivers/schedule','App\Http\Controllers\DriverController@getDriverS
 
 Route::post('swap/list/acceptall','App\Http\Controllers\SwapController@acceptall');
 
-Route::post('status-shipment', 'App\Http\Controllers\LogisticsController@getShipmentStatus');
+Route::post('shipments/status-shipment', 'App\Http\Controllers\LogisticsController@getShipmentStatus');
